98d1c0a0b6a9282d6628cad51a6089c03d4405d0
Snowbeast138 <snowbeast138@gmail.com>
2025-06-04 17:42:58 -0600

    TestFile

---
+++ benkychan/pages/Test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="font-bold text-3xl text-white bg-blue-600 px-8 py-4 rounded-lg shadow-lg">
+        Hello World
+      </h1>
+    </div>
+  );
+}

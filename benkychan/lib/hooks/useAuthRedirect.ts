37542f819153c90e61c655bc57b8427445ca6137
poteto03 <garciaariane03@gmail.com>
2025-06-11 21:20:03 -0600

    useAuthRedirect.ts documented

--- benkychan/lib/hooks/useAuthRedirect.ts
+++ benkychan/lib/hooks/useAuthRedirect.ts
@@ -3,17 +3,28 @@ import { auth } from "../../lib/firebase";
 import { useRouter } from "next/router";
 import { User } from "firebase/auth";
 
-export const useAuthRedirect = (requiredAuth: boolean = true) => {
+//Importa todas las dependencias necesarias
+
+/**
+ * Hook personalizado para manejar redirecciones basadas en el estado de autenticación
+ * Retorna un void
+ */
+
+export const useAuthRedirect = (requiredAuth: boolean = true) => { //Indica si la ruta actual requiere autenticación
   const router = useRouter();
 
   useEffect(() => {
+    // Suscripción a cambios en el estado de autenticación
     const unsubscribe = auth.onAuthStateChanged((user: User | null) => {
+      // Redirige a /login si se requiere autenticación y no hay usuario
       if (requiredAuth && !user) {
         router.push("/login");
-      } else if (!requiredAuth && user) {
+      } else if (!requiredAuth && user) { // Redirige a la raíz si no se requiere autenticación pero hay usuario logeado
         router.push("/");
       }
     });
+
+    // Función de limpieza para desuscribirse del listener de autenticación
     return () => unsubscribe();
-  }, [router, requiredAuth]);
+  }, [router, requiredAuth]); // Dependencias: router y requiredAuth
 };
